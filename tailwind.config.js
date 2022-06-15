97ac4c2ec4d2ef2391be659a9b84c9132446b25d
Cristian Iñiguez <felizpc1@gmail.com>
2022-06-15 18:44:11 -0400

    using vite-react

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/index.html'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       backgroundImage: {
