83120219ef5b9667d6837180f37df80ac072821c
Cristian Iñiguez <felizpc1@gmail.com>
2022-06-27 11:26:53 -0400

    proyecto: extraccion de componentes a clases para nuestras card

--- src/sections/Recommended.tsx
+++ src/sections/Recommended.tsx
@@ -1,7 +1,7 @@
 const Recommended = () => {
   return (
     <div className='p-20' id='recommended'>
-      <div className='w-48 h-64 rounded-lg overflow-hidden shadow-md'>
+      <div className='card'>
         <div className='h-3/5 bg-norway bg-cover' />
         <div className='h-2/5 bg-secondary text-white p-4'>
           <p className='font-bold text-xl'>Norway</p>
