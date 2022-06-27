a26a9008ad23c5cab9303c00752ddf9c2c5d719b
Cristian Iñiguez <felizpc1@gmail.com>
2022-06-27 12:10:45 -0400

    proyecto: agregando animcaciones al proyecto

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -3,8 +3,12 @@ const Home = () => {
     <section className='relative' id='home'>
       <img src='/img/sanFrancisco.jpg' alt='San Francisco' />
       <div className='w-full h-full flex flex-col justify-between items-center absolute top-0 py-8'>
-        <input className='p-3 rounded-full shadow-sm' placeholder='San Francisco' type='search' />
-        <button className='min-w-48 bg-white text-xl text-primary font-semibold p-4 rounded-full shadow-sm'>
+        <input
+          className='p-3 rounded-full shadow-sm transition duration-300 focus-within:shadow-sm focus:ring-2 focus:w-11/12'
+          placeholder='San Francisco'
+          type='search'
+        />
+        <button className='min-w-48 bg-white text-lg text-primary font-semibold p-4 rounded-full shadow-sm transition duration-500 ease-in-out hover:bg-primary hover:text-white transform hover:-translate-y-1 hover:scale-110'>
           Explore More
         </button>
       </div>
